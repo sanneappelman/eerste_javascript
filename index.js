440c91cc50111ca42a8bd8afec7d494dd02af1e1
sanneappelman <84176938+sanneappelman@users.noreply.github.com>
2021-05-29 17:37:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hello Winc Academy')
+
+
+let name = 'Sanne';
+console.log (name);
+
+
+//if kan niet, want dit is een officiele naam van iets (reserved keyword)
+
+
+console.log('4+4 kan, het antwoord is 8')
+console.log('de andere 4+4 kan ook. Hierop is het antwoord 44.')
+
+console.log('6*6=36')
+console.log('9*9=81')
+console.log ('80-30=50')
+console.log ('76+10=86')
+
+let age = 24; //Number Literal
+console.log ('age=number')
+
+let leeftijd = ('24')
+console.log ('leeftijd=string')
